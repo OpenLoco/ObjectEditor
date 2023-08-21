89137bee6f25680e80de5cb3500f91c362122676
Benjamin Sutas <benjamin.sutas@gmail.com>
2023-08-21 13:04:08 +1000

    add region graphics loading

--- OpenLocoTool/Objects/RegionObject.cs
+++ OpenLocoTool/Objects/RegionObject.cs
@@ -14,9 +14,26 @@ namespace OpenLocoTool.Objects
 		[property: LocoStructOffset(0x08)] uint8_t var_08,
 		[property: LocoStructOffset(0x09), LocoArrayLength(4)] uint8_t[] var_09,
 		[property: LocoStructOffset(0x0D), LocoArrayLength(0x12 - 0xD)] uint8_t[] pad_0D
-		) : ILocoStruct
+		) : ILocoStruct, ILocoStructExtraLoading
 	{
 		public static ObjectType ObjectType => ObjectType.region;
 		public static int StructSize => 0x12;
+
+		public ReadOnlySpan<byte> Load(ReadOnlySpan<byte> remainingData)
+		{
+			// unk
+			remainingData = remainingData[(ObjectHeader.SubHeaderLength * var_08)..];
+
+			// find all objects that this object loads
+			var ptr = 0;
+			while (remainingData[ptr] != 0xFF)
+			{
+				ptr += ObjectHeader.SubHeaderLength;
+			}
+			ptr++;
+			remainingData = remainingData[ptr..];
+
+			return remainingData;
+		}
 	}
 }

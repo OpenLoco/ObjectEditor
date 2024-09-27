d377915c5b825bfe81b3fc3ecc3ecfc740b2ec69
Benjamin Sutas <benjamin.sutas@gmail.com>
2024-09-27 12:23:34 +1000

    update build scripts for new gui folder name

--- build.sh
+++ build.sh
@@ -13,20 +13,18 @@ version="$1"
 echo "Building version $version"
 
 # 2. Write the version to version.txt. This is read by the UI to know the current version.
-echo "$version" > AvaGui/version.txt
+echo "$version" > Gui/version.txt
 
-## 2. Build the project for different platforms
+# 3. Build the project for different platforms
 echo "Building"
-dotnet publish AvaGui/AvaGui.csproj -c Release -p:PublishSingleFile=true -p:Version=$version --self-contained --runtime win-x64
-dotnet publish AvaGui/AvaGui.csproj -c Release -p:PublishSingleFile=true -p:Version=$version --self-contained --runtime linux-x64
-dotnet publish AvaGui/AvaGui.csproj -c Release -p:PublishSingleFile=true -p:Version=$version --self-contained --runtime osx-x64
+dotnet publish Gui/Gui.csproj -c Release -p:PublishSingleFile=true -p:Version=$version --self-contained --runtime win-x64
+dotnet publish Gui/Gui.csproj -c Release -p:PublishSingleFile=true -p:Version=$version --self-contained --runtime linux-x64
+dotnet publish Gui/Gui.csproj -c Release -p:PublishSingleFile=true -p:Version=$version --self-contained --runtime osx-x64
 
-# dotnet publish ObjectService/ObjectService.csproj -c Release -p:PublishSingleFile=true -p:Version=$version --self-contained --runtime linux-x64
-
-# 3. Create the ZIP and tar archives
+# 4. Create the ZIP and tar archives
 echo "Zipping"
 
-pushd "AvaGui/bin/Release/net8.0/"
+pushd "Gui/bin/Release/net8.0/"
 
 pushd "win-x64/publish"
 zip -r "object-editor-$version-win-x64.zip" .

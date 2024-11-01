6341478ec269e206f14cb515536dea3862219f1e
Benjamin Sutas <benjamin.sutas@gmail.com>
2024-11-02 00:24:02 +1100

    add more minimal ui validation to vehicle vm

--- Gui/ViewModels/DatTypes/VehicleViewModel.cs
+++ Gui/ViewModels/DatTypes/VehicleViewModel.cs
@@ -22,12 +22,12 @@ namespace OpenLoco.Gui.ViewModels
 		[Reactive, Category("Stats")] public uint8_t Reliability { get; set; }
 		[Reactive] public VehicleObjectFlags Flags { get; set; }
 		[Reactive] public object_id TrackTypeId { get; set; }
-		[Reactive, Category("Cost")] public uint8_t CostIndex { get; set; }
-		[Reactive, Category("Cost")] public int16_t CostFactor { get; set; }
-		[Reactive, Category("Cost")] public uint8_t RunCostIndex { get; set; }
-		[Reactive, Category("Cost")] public int16_t RunCostFactor { get; set; }
-		[Reactive] public BindingList<S5Header> CompatibleVehicles { get; set; }
-		[Reactive] public BindingList<S5Header> RequiredTrackExtras { get; set; }
+		[Reactive, Category("Cost"), Range(0, 32)] public uint8_t CostIndex { get; set; }
+		[Reactive, Category("Cost"), Range(1, int16_t.MaxValue)] public int16_t CostFactor { get; set; }
+		[Reactive, Category("Cost"), Range(0, 32)] public uint8_t RunCostIndex { get; set; }
+		[Reactive, Category("Cost"), Range(0, int16_t.MaxValue)] public int16_t RunCostFactor { get; set; }
+		[Reactive, Length(0, 8)] public BindingList<S5Header> CompatibleVehicles { get; set; }
+		[Reactive, Length(0, 4)] public BindingList<S5Header> RequiredTrackExtras { get; set; }
 		[Reactive] public uint8_t var_04 { get; set; }
 		[Reactive] public uint8_t var_113 { get; set; }
 		[Reactive, Category("Sprites")] public uint8_t ColourType { get; set; }

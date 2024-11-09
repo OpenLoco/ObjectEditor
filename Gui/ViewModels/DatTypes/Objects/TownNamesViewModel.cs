410bcb0232274a1164549a73ad097d32eb4b1f11
Benjamin Sutas <benjamin.sutas@gmail.com>
2024-11-10 01:06:49 +1100

    add airport vm

--- Gui/ViewModels/DatTypes/Objects/TownNamesViewModel.cs
+++ Gui/ViewModels/DatTypes/Objects/TownNamesViewModel.cs
@@ -1,3 +1,4 @@
+using OpenLoco.Dat.Data;
 using OpenLoco.Dat.Objects;
 using OpenLoco.Dat.Types;
 using ReactiveUI;
@@ -9,9 +10,11 @@ namespace OpenLoco.Gui.ViewModels
 {
 	public class TownNamesViewModel : ReactiveObject, IObjectViewModel<ILocoStruct>
 	{
-		[Reactive, Length(6, 6), Editable(false)] public BindingList<Category> Categories { get; set; }
+		[Reactive, Length(6, 6), Editable(false)]
+		public BindingList<Category> Categories { get; set; }
 
-		public TownNamesViewModel(TownNamesObject tno) => Categories = new(tno.Categories);
+		public TownNamesViewModel(TownNamesObject tno)
+			=> Categories = new(tno.Categories);
 
 		public ILocoStruct GetAsUnderlyingType(ILocoStruct locoStruct)
 			=> GetAsStruct((locoStruct as TownNamesObject)!);

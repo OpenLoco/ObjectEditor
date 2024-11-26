72f90a7a8d5d16c2df660679f2979e68242b1ab8
Benjamin Sutas <benjamin.sutas@gmail.com>
2024-11-26 11:34:16 +1100

    fix unit tests

--- Tests/LoadSaveTests.cs
+++ Tests/LoadSaveTests.cs
@@ -42,9 +42,11 @@ namespace OpenLoco.Dat.Tests
 			var logger = new Logger();
 			var (datFileInfo, locoObject) = SawyerStreamReader.LoadFullObjectFromStream(data, logger: logger);
 
+#pragma warning disable IDE0079 // Remove unnecessary suppression
 #pragma warning disable NUnit2045 // Use Assert.Multiple - cannot use a ReadOnlySpan inside an anonymous method
 			Assert.That(locoObject, Is.Not.Null);
 #pragma warning restore NUnit2045 // Use Assert.Multiple
+#pragma warning restore IDE0079 // Remove unnecessary suppression
 			Assert.That(datFileInfo.ObjectHeader.DataLength, Is.EqualTo(data.Length - S5Header.StructLength - ObjectHeader.StructLength), "ObjectHeader.Length didn't match actual size of struct");
 
 			return (locoObject!, (T)locoObject!.Object);
@@ -368,19 +370,19 @@ namespace OpenLoco.Dat.Tests
 				Assert.That(struc.BuildingAnimations[9].AnimationSpeed, Is.EqualTo(0));
 				// BuildingParts
 				Assert.That(struc.BuildingVariations, Has.Count.EqualTo(5));
-				Assert.That(struc.BuildingVariations[0], Has.Length.EqualTo(2));
+				Assert.That(struc.BuildingVariations[0], Has.Count.EqualTo(2));
 				Assert.That(struc.BuildingVariations[0][0], Is.EqualTo(0));
 				Assert.That(struc.BuildingVariations[0][1], Is.EqualTo(1));
-				Assert.That(struc.BuildingVariations[1], Has.Length.EqualTo(2));
+				Assert.That(struc.BuildingVariations[1], Has.Count.EqualTo(2));
 				Assert.That(struc.BuildingVariations[1][0], Is.EqualTo(2));
 				Assert.That(struc.BuildingVariations[1][1], Is.EqualTo(3));
-				Assert.That(struc.BuildingVariations[2], Has.Length.EqualTo(2));
+				Assert.That(struc.BuildingVariations[2], Has.Count.EqualTo(2));
 				Assert.That(struc.BuildingVariations[2][0], Is.EqualTo(4));
 				Assert.That(struc.BuildingVariations[2][1], Is.EqualTo(5));
-				Assert.That(struc.BuildingVariations[3], Has.Length.EqualTo(2));
+				Assert.That(struc.BuildingVariations[3], Has.Count.EqualTo(2));
 				Assert.That(struc.BuildingVariations[3][0], Is.EqualTo(6));
 				Assert.That(struc.BuildingVariations[3][1], Is.EqualTo(7));
-				Assert.That(struc.BuildingVariations[4], Has.Length.EqualTo(2));
+				Assert.That(struc.BuildingVariations[4], Has.Count.EqualTo(2));
 				Assert.That(struc.BuildingVariations[4][0], Is.EqualTo(8));
 				Assert.That(struc.BuildingVariations[4][1], Is.EqualTo(9));
 				// Rest of object
@@ -457,10 +459,10 @@ namespace OpenLoco.Dat.Tests
 				Assert.That(struc.BuildingAnimations[3].AnimationSpeed, Is.EqualTo(0));
 				// BuildingParts
 				Assert.That(struc.BuildingVariations, Has.Count.EqualTo(2));
-				Assert.That(struc.BuildingVariations[0], Has.Length.EqualTo(2));
+				Assert.That(struc.BuildingVariations[0], Has.Count.EqualTo(2));
 				Assert.That(struc.BuildingVariations[0][0], Is.EqualTo(0));
 				Assert.That(struc.BuildingVariations[0][1], Is.EqualTo(1));
-				Assert.That(struc.BuildingVariations[1], Has.Length.EqualTo(2));
+				Assert.That(struc.BuildingVariations[1], Has.Count.EqualTo(2));
 				Assert.That(struc.BuildingVariations[1][0], Is.EqualTo(2));
 				Assert.That(struc.BuildingVariations[1][1], Is.EqualTo(3));
 

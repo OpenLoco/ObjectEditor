dc045e2b31f350e536d9d99e8a8b0dce1db29420
Benjamin Sutas <benjamin.sutas@gmail.com>
2024-12-13 14:13:27 +1100

    clean up index asyncness, again

--- Common/Logging/LogLine.cs
+++ Common/Logging/LogLine.cs
@@ -3,6 +3,6 @@ namespace OpenLoco.Common.Logging
 	public record LogLine(DateTime Time, LogLevel Level, string Caller, string Message)
 	{
 		public override string ToString()
-			=> $"[{Time}] [{Level}] [{Caller}] {Message}";
+			=> $"[{Time:HH:mm:ss}] [{Level}] [{Caller}] {Message}";
 	}
 }

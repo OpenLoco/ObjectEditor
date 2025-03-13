dcb78cba743dbacf3d2e08d4cd2d603640b9a9ff
Benjamin Sutas <benjamin.sutas@gmail.com>
2025-03-13 16:28:54 +1100

    add simpler ui for support pillar placement

--- Gui/ViewModels/DatTypes/Objects/BridgeViewModel.cs
+++ Gui/ViewModels/DatTypes/Objects/BridgeViewModel.cs
@@ -1,18 +1,32 @@
 using OpenLoco.Dat.Objects;
 using PropertyModels.ComponentModel.DataAnnotations;
 using ReactiveUI.Fody.Helpers;
+using System;
 using System.ComponentModel;
 using System.Linq;
 
 namespace OpenLoco.Gui.ViewModels
 {
+	[Flags]
+	public enum SupportPillarSpacing : uint8_t
+	{
+		Tile0A = 1 << 0,
+		Tile0B = 1 << 1,
+		Tile1A = 1 << 2,
+		Tile1B = 1 << 3,
+		Tile2A = 1 << 4,
+		Tile2B = 1 << 5,
+		Tile3A = 1 << 6,
+		Tile4B = 1 << 7,
+	}
+
 	public class BridgeViewModel : LocoObjectViewModel<BridgeObject>
 	{
 		[Reactive, EnumProhibitValues<BridgeObjectFlags>(BridgeObjectFlags.None)] public BridgeObjectFlags Flags { get; set; }
 		[Reactive] public uint16_t ClearHeight { get; set; }
 		[Reactive] public int16_t DeckDepth { get; set; }
 		[Reactive] public uint8_t SpanLength { get; set; }
-		[Reactive] public uint8_t PillarSpacing { get; set; }
+		[Reactive] public SupportPillarSpacing PillarPlacement { get; set; }
 		[Reactive] public Speed16 MaxSpeed { get; set; }
 		[Reactive] public MicroZ MaxHeight { get; set; }
 		[Reactive, EnumProhibitValues<BridgeDisabledTrackFlags>(BridgeDisabledTrackFlags.None)] public BridgeDisabledTrackFlags DisabledTrackFlags { get; set; }
@@ -29,7 +43,7 @@ namespace OpenLoco.Gui.ViewModels
 		{
 			Flags = bo.Flags;
 			SpanLength = bo.SpanLength;
-			PillarSpacing = bo.PillarSpacing;
+			PillarPlacement = (SupportPillarSpacing)bo.PillarSpacing;
 			MaxSpeed = bo.MaxSpeed;
 			MaxHeight = bo.MaxHeight;
 			CostIndex = bo.CostIndex;
@@ -50,7 +64,7 @@ namespace OpenLoco.Gui.ViewModels
 			{
 				Flags = Flags,
 				SpanLength = SpanLength,
-				PillarSpacing = PillarSpacing,
+				PillarSpacing = (uint8_t)PillarPlacement,
 				MaxSpeed = MaxSpeed,
 				MaxHeight = MaxHeight,
 				CostIndex = CostIndex,

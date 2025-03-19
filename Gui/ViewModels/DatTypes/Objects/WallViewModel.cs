76676ac48374e92a8937cab51d1d70d052784c04
Benjamin Sutas <benjamin.sutas@gmail.com>
2025-03-19 17:28:02 +1100

    add wallviewmodel

---
+++ Gui/ViewModels/DatTypes/Objects/WallViewModel.cs
@@ -0,0 +1,32 @@
+﻿using OpenLoco.Dat.Objects;
+using PropertyModels.ComponentModel.DataAnnotations;
+using ReactiveUI.Fody.Helpers;
+
+namespace OpenLoco.Gui.ViewModels
+{
+	public class WallViewModel : LocoObjectViewModel<WallObject>
+	{
+		[Reactive] public uint8_t Height { get; set; }
+		[Reactive] public uint8_t ToolId { get; set; }
+
+		[Reactive, EnumProhibitValues<WallObjectFlags>(WallObjectFlags.None)] public WallObjectFlags Flags { get; set; }
+		[Reactive, EnumProhibitValues<WallObjectFlags2>(WallObjectFlags2.None)] public WallObjectFlags2 Flags2 { get; set; }
+
+		public WallViewModel(WallObject ro)
+		{
+			Height = ro.Height;
+			ToolId = ro.ToolId;
+			Flags = ro.Flags;
+			Flags2 = ro.Flags2;
+		}
+
+		public override WallObject GetAsStruct(WallObject tso)
+			=> tso with
+			{
+				Height = Height,
+				ToolId = ToolId,
+				Flags = Flags,
+				Flags2 = Flags2,
+			};
+	}
+}

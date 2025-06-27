d4b8604efe1bdedf87c6e66f08469ea2aa58407a
Benjamin Sutas <benjamin.sutas@gmail.com>
2025-06-27 16:24:19 +1000

    progress

---
+++ Definitions/Migrations/20250627061229_ReaddAvailability.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Definitions.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReaddAvailability : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Availability",
+                table: "Objects",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Availability",
+                table: "Objects");
+        }
+    }
+}

f44923d2ddf638429b831bd9145364dc9ac25e26
Benjamin Sutas <benjamin.sutas@gmail.com>
2025-09-06 14:34:42 +1000

    update wall image names

--- Definitions/ObjectModels/Objects/HillShapes/HillShapesObject.cs
+++ Definitions/ObjectModels/Objects/HillShapes/HillShapesObject.cs
@@ -1,10 +1,23 @@
 namespace Definitions.ObjectModels.Objects.HillShape;
 
-public class HillShapesObject : ILocoStruct
+public class HillShapesObject : ILocoStruct, IImageTableNameProvider
 {
 	public uint8_t HillHeightMapCount { get; set; }
 	public uint8_t MountainHeightMapCount { get; set; }
 	public bool IsHeightMap { get; set; }
 
 	public bool Validate() => true;
+
+	public bool TryGetImageName(int id, out string? value)
+		=> ImageIdNameMap.TryGetValue(id, out value);
+
+	public static Dictionary<int, string> ImageIdNameMap = new()
+	{
+		{ 0, "hill shape 1" },
+		{ 1, "hill shape 2" },
+		{ 2, "mountain shape 1" },
+		{ 3, "mountain shape 2" },
+		{ 4, "preview image" },
+
+	};
 }

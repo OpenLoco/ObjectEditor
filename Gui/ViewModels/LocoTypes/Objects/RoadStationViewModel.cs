37d2f22ce6827c1b4e155576c6c7f4aa488ec361
Benjamin Sutas <benjamin.sutas@gmail.com>
2025-09-22 10:21:01 +1000

    add cargo category to cargo offsets in road and track station

--- Gui/ViewModels/LocoTypes/Objects/RoadStationViewModel.cs
+++ Gui/ViewModels/LocoTypes/Objects/RoadStationViewModel.cs
@@ -21,9 +21,9 @@ public class RoadStationViewModel : LocoObjectViewModel<RoadStationObject>
 	[Category("Cost")] public int16_t SellCostFactor { get; set; }
 	[Category("Cost")] public uint8_t CostIndex { get; set; }
 	[Category("Compatible")] public List<ObjectModelHeaderViewModel> CompatibleRoadObjects { get; set; }
-	public ObjectModelHeader? CargoType { get; set; }
+	[Category("Cargo")] public ObjectModelHeader? CargoType { get; set; }
 
-	public CargoOffset[][][] CargoOffsets { get; init; }
+	[Category("Cargo")] public CargoOffset[][][] CargoOffsets { get; init; }
 
 	public RoadStationViewModel(RoadStationObject rso)
 	{
